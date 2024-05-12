6fe43a5612244a73fc18b1651769b210f34360c1
RianZenzei <132425448+Riansyah009@users.noreply.github.com>
2024-05-13 05:45:27 +0800

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,5 @@
 const shorten = require('../lib/sid')
+const tiktok = require('../lib/tiktok')
 const routes = async (fastify, options) => {
    fastify.get('/', async (req, res) => {
       return ({
@@ -17,6 +18,34 @@ const routes = async (fastify, options) => {
       })
       const json = await shorten(url)
       return res.send(json)
+   })
+}
+
+fastify.get('/tiktok', async (url) => {
+      const url = req.query.url
+      if (!url) return ({
+         creator: global.creator,
+         status: false,
+         msg: `"url" parameter required!`
+      })
+      const json = await tiktok(url)
+      return res.send({
+         status: true,
+         creator: global.creator,
+         data: {
+            nickname: json.data.author.nickname,
+            username: json.data.author.unique_id,
+            like: json.data.digg_count,
+            views: json.data.play_count,
+            size: json.data.size,
+            caption: json.data.title,
+            nowm: "https://tikwm.com" + json.data.play,
+            nowmhd: "https://tikwm.com" + json.data.hdplay,
+            wm: "https://tikwm.com" + json.data.wmplay,
+            music: "https://tikwm.com" + json.data.music,
+            images: json.data.images
+         }
+      })
    })
 }
 
