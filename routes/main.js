3b0a89875b8a2ea6a48e7dae0a53b6968467f9cf
RianZenzei <132425448+Riansyah009@users.noreply.github.com>
2024-05-13 05:53:21 +0800

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -9,19 +9,7 @@ const routes = async (fastify, options) => {
       })
    })
 
-   fastify.get('/short', async (req, res) => {
-      const url = req.query.url
-      if (!url) return ({
-         creator: global.creator,
-         status: false,
-         msg: `"url" parameter required!`
-      })
-      const json = await shorten(url)
-      return res.send(json)
-   })
-}
-
-fastify.get('/tiktok', async (value) => {
+   fastify.get('/tiktok', async (value) => {
       const url = req.query.url
       if (!url) return ({
          creator: global.creator,
@@ -47,6 +35,20 @@ fastify.get('/tiktok', async (value) => {
          }
       })
    })
+
+
+   fastify.get('/short', async (req, res) => {
+      const url = req.query.url
+      if (!url) return ({
+         creator: global.creator,
+         status: false,
+         msg: `"url" parameter required!`
+      })
+      const json = await shorten(url)
+      return res.send(json)
+   })
 }
+
+\
 
 module.exports = routes
